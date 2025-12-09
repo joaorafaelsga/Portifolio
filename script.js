cacb5cd90d1a379b0dff7169e2950df5dc5747fc
João Rafael Alcoforado <joaorafael.alcoforado@gmail.com>
2025-12-09 18:26:41 -0300

    Add click event to open easteregg.html

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    const imagem = document.getElementById('cachorro.jpg');
+    const urlParaAbrir = 'easteregg.html'; 
+
+    imagem.addEventListener('click', function() {
+        window.open(urlParaAbrir, '_blank'); 
+        
+    });
+});
 
